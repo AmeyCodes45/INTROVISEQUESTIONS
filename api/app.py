d9a5a754763ae05622e164453989a243a66c2bf2
AmeyCodes45 <codinghelps45@gmail.com>
2025-03-01 16:54:33 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -12,7 +12,7 @@ DATA_FOLDER = os.path.join(os.path.dirname(__file__), "data")
 tech_csv_files = {
     "software developer": os.path.join(DATA_FOLDER, "software_developer.csv"),
     "data analyst": os.path.join(DATA_FOLDER, "data_analyst.csv"),
-    "aiml engineer": os.path.join(DATA_FOLDER, "ai_ml_engineer.csv"),
+    "aimlengineer": os.path.join(DATA_FOLDER, "ai_ml_engineer.csv"),
     "developer engineer": os.path.join(DATA_FOLDER, "developer_engineer.csv"),
     "cloud engineer": os.path.join(DATA_FOLDER, "cloud_engineer.csv")
 }
